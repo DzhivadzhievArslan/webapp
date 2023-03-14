d255e75f734395e9aef3682e452a4024581a00d9
Arslan <arsymaster@gmail.com>
2023-03-14 17:38:34 +0300

    Created first app (main)

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('<h4>Проверка работы</h4>')
+
+def about(request):
+    return HttpResponse('<h4>Страница о нас</h4>')
+
+def inspiration(request):
+    return HttpResponse('<h3>Арсленго, ты станешь гуру в области разработки!</h3>')
